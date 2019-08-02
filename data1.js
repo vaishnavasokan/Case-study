4f528fd0c57e0ce42e97fa6d4779c9b1998df318
vaishnavasokan <vaishnavasokan050>
2019-08-02 10:48:12 +0530

    commit5

--- data1.js
+++ data1.js
@@ -8,19 +8,19 @@ $(document).ready(function()
             url:"https://jsonplaceholder.typicode.com/todos",
             success:function(p)
             {
-                var output="<table class='table'><thead><th>ID</th><th>TITLE</th></thead>";
+                var output="<table class='table'><thead style='background:#66a3ff'><th style='border-right: 1px solid black;'>ID</th><th style='border-right: 1px solid black;'>TITLE</th><th style='border-right: 1px solid black; text-align: center'>CHECKLIST</th></thead>";
 
                 for(var i in p)               
                 {
                     output+="<tr>";
-                    output+="<td>"+  p[i].id+"</td>";
-                    output+="<td>"+ p[i].title+"</td>";
+                    output+="<td style='border-right: 1px solid black;'>"+  p[i].id+"</td>";
+                    output+="<td style='border-right: 1px solid black;'>"+ p[i].title+"</td>";
                     if(p[i].completed==true)
                     {
-                        output+="<td><input type='checkbox' checkbox checked id='cb1' disabled></td></tr>";
+                        output+="<td style='text-align: center;'><input type='checkbox' checkbox checked id='cb1' disabled></td></tr>";
                     }
                     else
-                    output+="<td><input type='checkbox' id='cb1'></td></tr>";
+                    output+="<td style='text-align: center;'><input type='checkbox' id='cb1'></td></tr>";
                     
                     //output+="<td>"+p[i].publishedAt+"</td>";
                 }
@@ -33,6 +33,7 @@ $(document).ready(function()
                     if($(this).prop("checked") == true)
                     {
                         c+=1;
+
                     }    
 
                     var mypromise = new Promise((resolve,reject)=>
